33afd48cef660111b9d5de0ef0dd927dffd8aa76
Евгения <93819797+EvgeniyaTemereva@users.noreply.github.com>
2022-02-11 14:58:23 +0300

    Add files via upload

---
+++ Lesson1.1.py
@@ -0,0 +1,11 @@
+a = "The"
+b = "Developer’s"
+c = "Dilemma"
+print(f"{a} {b} {c}")
+numb1 = int(input("Enter any number: "))
+numb2 = int(input("Enter any number one more time: "))
+print(f"You have chosen the numbers {numb1} and {numb2}")
+word = input(" What can I do for you this morning?: ")
+print(f"{word} - Hey, watch out!")
+
+
