3b673584cb059d5ea6f0ca4319230f27bfa51d8c
Евгения <93819797+EvgeniyaTemereva@users.noreply.github.com>
2022-03-05 22:34:06 +0300

    Add files via upload

---
+++ 2.py
@@ -0,0 +1,25 @@
+"""
+Создайте собственный класс-исключение, обрабатывающий ситуацию деления на ноль.
+Проверьте его работу на данных, вводимых пользователем. При вводе пользователем нуля в
+качестве делителя программа должна корректно обработать эту ситуацию и не завершиться с
+ошибкой.
+"""
+
+
+class MyException(Exception):
+
+    def division_func(self, a, b):
+        try:
+            res = round(a / b, 2)
+        except ZeroDivisionError:
+            print(f"{a} / {b} -> на ноль делить нельзя!\n")
+        else:
+            print(f"{a} / {b} = {res} \n")
+
+
+m_e = MyException()
+
+m_e.division_func(4, 2)
+m_e.division_func(5, 0)
+m_e.division_func(-7, 7)
+m_e.division_func(0, 9)
